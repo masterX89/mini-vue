409b1cfaab3f2e2aedd23c0ac38a191a39001bfb
hk.xia <595424932@qq.com>
2022-05-05 11:37:00 +0800

    refactor(runtime-core): async isFlushPending

--- examples/nextTicker/App.js
+++ examples/nextTicker/App.js
@@ -5,9 +5,11 @@ export default {
     const count = ref(0)
     const onClick = () => {
       console.log('onClick')
-      for (let i = 0; i < 100; i++) {
+      for (let i = 0; i < 5; i++) {
         count.value = i
       }
+      // 此时视图由于异步还没有更新
+      console.log('count: ', document.getElementById('counter').textContent)
     }
     return {
       count,
@@ -16,7 +18,7 @@ export default {
   },
   render() {
     return h('div', {}, [
-      h('p', {}, `${this.count}`),
+      h('p', { id: 'counter' }, `${this.count}`),
       h(
         'button',
         {
