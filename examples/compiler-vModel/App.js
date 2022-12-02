93f8ff73fa9993e841cebcc41c18aede39f4f220
masterX89 <xhk1995@gmail.com>
2022-12-02 10:20:32 +0800

    chore(vModel): add TODO

--- examples/compiler-vModel/App.js
+++ examples/compiler-vModel/App.js
@@ -1,3 +1,6 @@
+// 回想 React16 单向绑定: 需要在 onChange 中 setState 修改值
+// 而 vue3 使用 v-model 指令可以双向绑定, 选择该指令是由于树的结构相比 v-if 是稳定的, 不涉及 Block
+// TODO: 1. runtime 之中完成双向绑定 2. compile 中完成编译
 import { ref } from '../../lib/mini-vue.esm.js'
 export const App = {
   name: 'App',
